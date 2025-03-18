666c395fbc3030a78e91ae48b093c6e29380fd7c
Driptanil Roy <153745351+driptanilroy@users.noreply.github.com>
2025-03-18 21:00:13 +0530

    Create Question1.c of PracticeLab2 of Week 3

---
+++ week3/PracticeLab2/Question1.c
@@ -0,0 +1,41 @@
+/*
+PracticeLab2_Question1
+
+1. Please find the output of a C program intended to print a pattern using integers. The pattern has N rows, where N is
+input from the user. The first row begins with the number N and prints all numbers from N to 1 in decreasing order. The
+second row starts with the number N-1, and so on. The last row has the single number 1. The following is the inverted
+right triangle pattern for N=5:
+
+Correct Output for N=5:
+54321
+4321
+321
+21
+1
+
+
+The program provided in the file has a semantic error, and it is printing the following pattern for N = 5:
+
+Wrong Output for N=5:
+1
+21
+321
+4321
+54321
+
+
+Semantic error is different from syntactic error. It means the C code will successfully compile without error but will
+not produce the intended results. Modify the provided code to rectify the logical error.
+
+2. Modify the program slightly to print the following pattern as well:
+
+54321
+5432
+543
+54
+5
+
+*/
+
+
+//CODE
